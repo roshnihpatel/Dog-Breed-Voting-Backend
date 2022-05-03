ada479c5d0febc72ab9021beba014279c416b628
sajsiv <sajjansivia@gmail.com>
2022-05-03 10:52:22 +0100

    change env var

--- server.ts
+++ server.ts
@@ -27,7 +27,8 @@ const client = new Client(dbConfig);
 client.connect();
 
 app.get("/", async (req, res) => {
-  const dbres = await client.query('select * from categories');
+  const dbres = await client.query('select breed from dogs');
+  console.log(dbres.rows)
   res.json(dbres.rows);
 });
 
